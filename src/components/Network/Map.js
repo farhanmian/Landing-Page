89cb91fd1c144bcae09654d7e825115ab5311d8b
farhanmian <farhanmian099@gmail.com>
2021-11-02 15:22:32 +0530

    google maps added

--- src/components/Network/Map.js
+++ src/components/Network/Map.js
@@ -1,11 +1,11 @@
 import mapImg from '../../assets/img/map.png';
-import classes from './Map.module.css';
+import cssClasses from './Map.module.css';
 
 export default function Map() {
     return (
-        <div className={classes.map}>
-            <img className={classes.img} src={mapImg} alt="map" />
-            {/* <iframe classname={classes.iframe} src="https://www.google.com.qa/maps/d/u/0/embed?mid=1BeyAdnsIQBZ3y6agh4owzM4lLNtqDY-o" width="100%" height="100%"></iframe> */}
+        <div className={cssClasses.map}>
+            {/* <img className={classes.img} src={mapImg} alt="map" /> */}
+            <iframe className={cssClasses.iframe} src="https://www.google.com.qa/maps/d/u/0/embed?mid=1BeyAdnsIQBZ3y6agh4owzM4lLNtqDY-o" width="100%" height="120%"></iframe>
         </div>
     )
 }
